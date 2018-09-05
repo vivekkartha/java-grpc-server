49aee8b03deaaeb17c831890186e68459a4d4867
Vivek Kartha <vivek.kartha@mobme.in>
2018-09-05 18:24:58 +0530

    Adding all files

---
+++ src/main/java/com/molten/grpc/App.java
@@ -0,0 +1,19 @@
+package com.molten.grpc;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8080)
+                .addService(new PersonServiceImpl())
+                .build();
+        server.start();
+        // Server threads are running in the background.
+        System.out.println("Server started");
+        // Don't exit the main thread. Wait until server is terminated.
+        server.awaitTermination();
+    }
+}
